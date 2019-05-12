0c74300b912a5715424d5539d956d1064b8698ba
Ala <alicja.anszpergier@outlook.com>
2019-05-12 17:11:42 +0200

    klasa Game uzupełniona o podstawowe elementy

--- TP_2019/CasinoLibrary/Game.cs
+++ TP_2019/CasinoLibrary/Game.cs
@@ -8,5 +8,34 @@ namespace CasinoDataModelLibrary
 {
     class Game
     {
+        private int Id;
+        private int maxPlayers;
+        private int minPlayers;
+        private Double maxPrize;
+        private Double minBet;
+
+        public int Id1
+        {
+            get => Id;
+            set => Id = value;
+        }
+
+        public int MaxPlayers
+        {
+            get => maxPlayers;
+            set => maxPlayers = value;
+        }
+
+        public int MinPlayers
+        {
+            get => minPlayers;
+            set => minPlayers = value;
+        }
+
+        public double MinBet
+        {
+            get => minBet;
+            set => minBet = value;
+        }
     }
 }
