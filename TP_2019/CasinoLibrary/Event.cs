cbbdb0b6043bcbaa8d5555f852433ef303418f72
DobromirKata <45312230+DobromirKata@users.noreply.github.com>
2019-05-12 18:46:06 +0200

    Poprawki

--- TP_2019/CasinoLibrary/Event.cs
+++ TP_2019/CasinoLibrary/Event.cs
@@ -8,5 +8,38 @@ namespace CasinoDataModelLibrary
 {
     class Event
     {
+        private CurrentGame currentGame;
+        private User user;
+        private DateTimeOffset startGameTime;
+        private DateTimeOffset endGameTime;
+
+        public CurrentGame CurrentGame
+        {
+            get => currentGame;
+            set => currentGame = value;
+        }
+
+        public User User
+        {
+            get => user;
+            set => user = value;
+        }
+
+        public DateTimeOffset StartGameTime
+        {
+            get => startGameTime;
+            set => startGameTime = value;
+        }
+
+        public DateTimeOffset EndGameTime
+        {
+            get => endGameTime;
+            set => endGameTime = value;
+        }
+
+
+
+
+
     }
 }
