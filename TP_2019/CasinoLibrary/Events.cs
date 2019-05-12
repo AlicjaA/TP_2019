5e1bd8c066f86a8a6d41af3aecd6f6cc46c14c8f
DobromirKata <45312230+DobromirKata@users.noreply.github.com>
2019-05-12 18:13:41 +0200

    Dodanie klasy Events

---
+++ TP_2019/CasinoLibrary/Events.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoDataModelLibrary
+{
+    class Events
+    {
+        private CurrentGame currentGame;
+        private User user;
+        private DataTimeOffset startGameTime;
+        private DateTimeOffset endGameTime;  //przy deklaracj zmiennej ? pole moze miec wartosc null
+
+
+        public CurrentGame CurrentGame
+        {
+            get => currentGame;
+            set => currentGame => value;
+        }
+
+        public User User
+        {
+            get => user;
+            set => user => value;
+        }
+
+        public DateTimeOffset startGameTime;
+        {        
+            get => startGameTime;
+            set => startGameTime => value;
+        }
+
+
+    }
+}
