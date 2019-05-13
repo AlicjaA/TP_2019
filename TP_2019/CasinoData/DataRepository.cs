dad7dbd91c6bb549d03f0c74f83e8ac5ef8a78f2
DobromirKata <45312230+DobromirKata@users.noreply.github.com>
2019-05-13 12:03:42 +0200

    Dodanie klas DataRepository i DataFiller

---
+++ TP_2019/CasinoData/DataRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoData
+{
+    public class DataRepository
+    {
+        private DataFiller filler;
+        private DataContext data;
+
+        public DataRepository(DataFiller dataFiller)
+        {
+            this.filler = dataFiller;
+        }
+
+        public DataContext Data
+        {
+            set => data = value;
+        }
+
+        public void Fill()
+        {
+            filler.Fill(ref data);
+        }
+
+
+
+
+
+
+    }
+}
