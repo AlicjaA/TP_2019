de44ecca4a31b9136297d8de854bf7aa1ca21362
DobromirKata <45312230+DobromirKata@users.noreply.github.com>
2019-05-13 12:16:38 +0200

    ConstDataFiller

---
+++ TP_2019/CasinoData/ConstDataFiller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoData
+{
+    public class ConstDataFiller : DataFiller
+    {
+        public override void Fill(ref DataContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
