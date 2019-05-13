4fae6850e6da6c38274b5d699c8a3844bc00a554
DobromirKata <45312230+DobromirKata@users.noreply.github.com>
2019-05-13 11:03:26 +0200

    Dodanie amiennych i metod do DataContext

--- TP_2019/CasinoData/DataContext.cs
+++ TP_2019/CasinoData/DataContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,14 @@ namespace CasinoData
 {
     public class DataContext
     {
+        public List<User> users;
+
+        public Dictionary<string, Game> games;
+
+        public ObservableCollection<Event> events;
+
+        public List<CurrentGame> currentGames;
+
+
     }
 }
