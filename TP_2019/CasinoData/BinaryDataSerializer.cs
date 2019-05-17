446da5d6103b19f090b8ab22499f2d5c5f40297b
DobromirKata <45312230+DobromirKata@users.noreply.github.com>
2019-05-18 00:08:09 +0200

    Utworzenie klasy BinaryDataSerializer

--- TP_2019/CasinoData/BinaryDataSerializer.cs
+++ TP_2019/CasinoData/BinaryDataSerializer.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CasinoData
 {
-    class BinaryDataSerializer
+    public class BinaryDataSerializer : IDataSerializer
     {
+        private string fileName;
+
+        public void Deserialize(ref DataContext context)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            using (Stream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                formatter.Serialize(stream, context);
+                stream.Flush();
+            }
+        }
+
+        public void Serialize(DataContext context)
+        {
+            IFormatter formatter = new BinaryFormatter();
+
+            //check if file exist
+            if (File.Exists(fileName))
+            {
+
+
+                context = null;
+                using (Stream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    context = (DataContext) formatter.Deserialize(stream);
+                }
+            }
+            else
+
+            {
+                throw new FileNotFoundException("Nie znaleziono pliku");
+            }
+        }
     }
 }
