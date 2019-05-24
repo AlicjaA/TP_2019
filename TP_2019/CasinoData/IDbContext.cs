6a831661115488d1a7a5bb00002cc7d497d44e69
DobromirKata <45312230+DobromirKata@users.noreply.github.com>
2019-05-25 01:47:09 +0200

    Dodanie projektu WPF CasinoApplication, Stworzenie wzorca MVVM, dodanie interfejsu IDbContext

---
+++ TP_2019/CasinoData/IDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CasinoDataModelLibrary;
+using System.Data.Entity;
+
+namespace CasinoData
+{
+    public interface IDbContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Game> Games { get; set; }
+        DbSet<CurrentGame> CurrentGames {get; set; }
+        DbSet<Event> Events { get; set; }
+        void SaveChanges();
+    }
+}
