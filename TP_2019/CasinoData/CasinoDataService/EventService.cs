5bee237872b012cf7942b80556568c33d49dee9c
DobromirKata <45312230+DobromirKata@users.noreply.github.com>
2019-05-25 13:35:02 +0200

    Dodanie EventService.cs

---
+++ TP_2019/CasinoData/CasinoDataService/EventService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CasinoDataModelLibrary;
+
+namespace CasinoData
+{
+    public partial class CasinoDataService
+    {
+        public string PrintEvents(List<Event> events)
+        {
+            string eventString = "";
+
+            foreach (Event event in events)
+            {
+                eventString += events;
+
+                if (events.LastIndexOf(event) != events.Count)
+                {
+                    eventString += ", ";
+                }
+            }
+
+            return eventString;
+        }
+
+        public void AddEvents(Event event)
+        {
+            if (!casinoDataRepository.GetAllEvents().Contains(event))
+            {
+                casinoDataRepository.AddEvent(events:);
+            }
+            else
+            {
+                throw new Exception("Participation alredy exists!");
+            }
+        }
+    }
+}
