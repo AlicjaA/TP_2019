777d1efa6e14ea3c624f9eb463bbe6804f3bb5ea
DobromirKata <45312230+DobromirKata@users.noreply.github.com>
2019-05-25 02:14:15 +0200

    Dodanie CasinoDataRepository.cs

---
+++ TP_2019/CasinoData/CasinoDataRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoData
+{
+    public partial class CasinoDataRepository
+    {
+        private IDbContext dataContext;
+
+        public IDbContext DataContext
+        {
+            set => dataContext = value;
+        }
+
+        public CasinoDataRepository(IDbContext dbContext)
+        {
+            dataContext = dbContext;
+        }
+    }
+}
