88d0d2d85fcc620a8ed42942d5c14af7c3c047e6
DobromirKata <45312230+DobromirKata@users.noreply.github.com>
2019-05-25 02:47:19 +0200

    CRUD for UsersRepository.cs

---
+++ TP_2019/CasinoData/CasinoDataRepository/UsersRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CasinoDataModelLibrary;
+
+namespace CasinoData
+{
+    public partial class CasinoDataRepository
+    {
+        public void AddUser(User user)
+        {
+            dataContext.Users.Add(user);
+            dataContext.SaveChanges();
+        }
+
+        public User GetUser(int id)
+        {
+            return dataContext.Users.FirstOrDefault(uId => uId.ID == id);
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return dataContext.Users;
+        }
+
+        public void UpdateUser(User oldUser, User newUser)
+        {
+            oldUser.FirstName = newUser.FirstName;
+            oldUser.LastName = newUser.LastName;
+            dataContext.SaveChanges();
+        }
+
+        public bool DeleteUser(User user)
+        {
+            dataContext.Users.Remove(user);
+            dataContext.SaveChanges();
+            return true;
+        }
+
+    }
+}
