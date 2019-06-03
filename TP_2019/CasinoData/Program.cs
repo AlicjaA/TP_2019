545c675636b22fa68d53c0affe2152a39ff9e98a
DobromirKata <45312230+DobromirKata@users.noreply.github.com>
2019-06-03 13:27:25 +0200

    Utworzenie CasinoDataRepositoryTest i CasinoDataContextTest

---
+++ TP_2019/CasinoData/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using CasinoData;
+using CasinoDataModelLibrary;
+
+
+//Alicja Anszpergier 195755
+//Dobromir Kata 176555
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataContext dataContext = new DataContext();
+
+            //DataFiller dataFiller = new XmlDataFiller()
+            //{
+            //    FileName = "../../../XMLDataContext.xml"
+            //};
+
+
+            //DataFiller dataFiller = new ConstDataFiller();
+
+            ///*
+            DataFiller dataFiller = new RandomDataFiller()
+            {
+                NumberOfGames = 10,
+                NumberOfCurrentGames = 10,
+                NumberOfUsers = 10,
+                NumberOfEvents = 10
+            };
+            //*/
+            DataRepository dataRepository = new DataRepository(dataFiller)
+            {
+                Data = dataContext
+            };
+            dataRepository.Fill();
+
+            DataService service = new DataService(dataRepository);
+
+            Console.WriteLine(service.PrintAllBinded());
+
+            FileStream writer = new FileStream("XMLDataContext.xml", FileMode.Create);
+
+            //dodanie typów, które ma rozpoznać DataContractSerializer
+            List<Type> types = new List<Type> { typeof(Event), typeof(Game), typeof(User), typeof(CurrentGame) };
+            var dataContractSerializer = new DataContractSerializer(typeof(DataContext), types, 0x7FFF, false, true, null);
+            dataContractSerializer.WriteObject(writer, dataContext);
+
+            writer.Flush();
+            writer.Close();
+
+
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ TP_2019/UnitTestCasino/CasinoDataRepositoryTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CasinoData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestCasino
+{
+    [TestClass]
+    public class CasinoDataRepositoryTest
+    {
+        CasinoDataRepository dataRepository;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            IDbContext dbContext = new TestCasinoDataContext();
+            dataRepository = new CasinoDataRepository(dbContext);
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
