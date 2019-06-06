b3a1845b017a939fb3a50712eae9fc4a7022a507
DobromirKata <45312230+DobromirKata@users.noreply.github.com>
2019-06-06 04:47:44 +0200

    Dodanie do warstwy Model CasinoData

---
+++ TP_2019/GUI/Model/CasinoData.cs
@@ -0,0 +1,32 @@
+﻿using CasinoData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.Model
+{
+    public static class CasinoData
+    {
+        private static CasinoDataRepository dataRepository;
+
+        public static CasinoDataRepository CasinoDataRepository
+        {
+            get => dataRepository;
+        }
+
+        private static CasinoDataService dataService;
+
+        public static CasinoDataService CasinoDataService
+        {
+            get => dataService;
+        }
+
+        public static void RegDataRepository(CasinoDataRepository dataRepository)
+        {
+            dataService = new CasinoDataService(dataRepository);
+        }
+
+    }
+}
