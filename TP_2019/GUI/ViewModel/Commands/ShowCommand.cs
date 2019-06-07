c2fa65d440336be041036803047aab48c926be74
Ala <alicja.anszpergier@outlook.com>
2019-06-07 21:52:45 +0200

    listy p1

---
+++ TP_2019/GUI/ViewModel/Commands/ShowCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GUI.ViewModel.Commands
+{
+    public class ShowCommand : ICommand
+    {
+        readonly Action<object> execute;
+        readonly Predicate<object> canExecute;
+
+        public ShowCommand(Action<object> executeDelegate, Predicate<object> canExecuteDelegate)
+        {
+            execute = executeDelegate;
+            canExecute = canExecuteDelegate;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute(this);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged.Invoke(this, null);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(this);
+        }
+    }
+
+}
+
