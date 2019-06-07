26ff0f7345775e39f9c6cb178c445157749b60ac
Ala <alicja.anszpergier@outlook.com>
2019-06-07 22:44:58 +0200

    klasy command + addCommand

---
+++ TP_2019/GUI/ViewModel/Commands/AddCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GUI.ViewModel.Commands
+{
+    public class AddCommand : ICommand
+    {
+        #region Fields
+        private Action<object> execute;
+        #endregion
+
+        #region Constructors
+        public AddCommand(Action<object> executeDelegate)
+        {
+            execute = executeDelegate;
+        }
+        #endregion
+
+        #region ICommandsMembers
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(this);
+        }
+
+        #endregion
+
+    }
+}
